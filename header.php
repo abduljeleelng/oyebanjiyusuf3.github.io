7e02b00170716949465c7d01f7ad6725c60dfbae
oyebanji_yusuf <oyebanjiyusuf3@gmail.com>
2019-06-15 22:36:16 +0100

    pushing home and About

---
+++ header.php
@@ -0,0 +1,21 @@
+ <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+        <img src="images/qefas.png" alt="" className="brand-image img-circle elevation-3" height="80" width="60">
+	      <a class="navbar-brand" href="index.php"><h2>QEFAS</h2></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="domain.html" class="nav-link">Domain</a></li>
+	          <li class="nav-item"><a class="nav-link" href="hosting.html">Hosting</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>Get started</span></a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Qefas - Online Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+	 <?php
+   require_once('header.php');
+   ?>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight">
+      <div class="overlay"></div>
+      <div class="container-fluid px-0">
+      	<div class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end">
+	      	<img class="one-third js-fullheight align-self-end order-md-last img-fluid" src="images/undraw_pair_programming_njlp.svg" alt="">
+	        <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
+	        	<div class="text mt-5">
+	        		<span class="subheading">Qefas Prep School</span>
+	            <h2 class="mb-3"><span>Online Tutor</span><span>,Development</span><span>,Hosting</span></h2>
+	            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                quas necessitatibus.</p>
+	            <p><a href="#" class="btn btn-primary px-4 py-3">Get in touch</a></p>
+	          </div>
+	        </div>
+	    	</div>
+      </div>
+    </div>
+    
+    
+  
+    <section class="ftco-section services-section bg-light">
+      <div class="container">
+      	<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+            <h2 class="mb-4">Why Choose Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                quas necessitatibus</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center order-md-last">
+            		<span class="flaticon-cloud"></span>
+            	</div>
+              <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                <h3 class="heading">Online Tutor</h3>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                    quas necessitatibus</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center">
+            		<span class="flaticon-server"></span>
+            	</div>
+              <div class="media-body pl-4">
+                <h3 class="heading">Professional Teachers</h3>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                    quas necessitatibus</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center order-md-last">
+            		<span class="flaticon-customer-service"></span>
+            	</div>
+              <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                <h3 class="heading">Technical Services</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                    quas necessitatibus</p>
+              </div>
+            </div>      
+          </div>
+					<div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center">
+            		<span class="flaticon-life-insurance"></span>
+            	</div>
+              <div class="media-body pl-4">
+                <h3 class="heading">Responsive Design</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                    quas necessitatibus</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center order-md-last">
+            		<span class="flaticon-cloud-computing"></span>
+            	</div>
+              <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                <h3 class="heading">Quality Product</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus 
+                    quas necessitatibus</p>
+              </div>s
+            </div>    
+          </div>
+          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-flex align-items-center">
+            	<div class="icon d-flex align-items-center justify-content-center">
+            		<span class="flaticon-settings"></span>
+            	</div>
+              <div class="media-body pl-4">
+                <h3 class="heading">24/7 Support</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section ftco-partner">
+    	<div class="container">
+          <div class="col-md-12 text-center heading-section heading-section-black ftco-animate">
+              <span class="subheading">Other Company Under QEFAS</span>
+            </div>
+        <div class="row">
+            <div class="col-sm ftco-animate">
+              <a href="#" class="partner"><img src="images/partner-1.png" class="img-fluid" alt="Colorlib Template"></a>
+            </div>
+            <div class="col-sm ftco-animate">
+              <a href="#" class="partner"><img src="images/partner-2.png" class="img-fluid" alt="Colorlib Template"></a>
+            </div>
+            <div class="col-sm ftco-animate">
+              <a href="#" class="partner"><img src="images/partner-3.png" class="img-fluid" alt="Colorlib Template"></a>
+            </div>
+            <div class="col-sm ftco-animate">
+              <a href="#" class="partner"><img src="images/partner-4.png" class="img-fluid" alt="Colorlib Template"></a>
+            </div>
+            <div class="col-sm ftco-animate">
+              <a href="#" class="partner"><img src="images/partner-5.png" class="img-fluid" alt="Colorlib Template"></a>
+            </div>
+        
+    	</div>
+    	</div>
+    </section>
+
+ 
+  <?php
+  require_once('footer.php');
+
+  ?>
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
